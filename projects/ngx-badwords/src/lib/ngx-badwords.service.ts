2654be910f3a2f74bc35586f838975b1f494fde0
Srinivas Tamada <srinivas@9lessons.info>
2020-04-25 21:05:05 -0400

    A

--- projects/ngx-badwords/src/lib/ngx-badwords.service.ts
+++ projects/ngx-badwords/src/lib/ngx-badwords.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import Filter from 'bad-words';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NgxBadwordsService {
+  filter: any;
+  constructor() {
+    this.filter = new Filter();
+  }
 
-  constructor() { }
+  clean(text: string) {
+    return this.filter.clean(text);
+  }
 }
